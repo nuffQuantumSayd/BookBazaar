cdedf2a33b1c4a39259071336413ed3866f7917a
aaron <aaronkornish.ak@gmail.com>
2024-01-06 17:46:07 -0800

    Created an enum of genres

---
+++ BookBazaar/Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿namespace BookBazaar.Models
+{
+    public enum Genre
+    {
+        Fiction,
+        NonFiction,
+        Fantasy,
+        Mystery,
+        Romance,
+        Thriller,
+        Horror,
+        Biography,
+        History,
+        Science,
+        Children,
+        Other
+    }
+    
+}
