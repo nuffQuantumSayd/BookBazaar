69dabd99b9420a0742e56a09b149fae13f0f8f43
aaron <aaronkornish.ak@gmail.com>
2024-01-10 13:14:08 -0800

    Created a shopping cart with CRUD functions

---
+++ BookBazaar/Models/ShoppingCart.cs
@@ -0,0 +1,37 @@
+﻿namespace BookBazaar.Models
+{
+    public class ShoppingCart
+    {
+        public int CartId { get; set; }
+        public List<CartItem> CartItems { get; set; }
+
+        public double TotalPrice { get; set; }
+
+
+        public ShoppingCart()
+        {
+            CartItems = new List<CartItem>();
+        }
+
+        public void AddItem(CartItem item)
+        {
+            CartItems.Add(item);
+        }
+
+        public void RemoveItem(CartItem item)
+        {
+            CartItems.Remove(item);
+        }
+
+        public void ClearCart()
+        {
+            CartItems.Clear();
+        }
+
+        public void UpdateCart(CartItem item)
+        {
+            CartItems[CartItems.FindIndex(i => i.BookId == item.BookId)] = item;
+        }
+    }
+    
+}
