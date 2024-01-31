7533d26129c0162afaac12d53a754fd5e7b07c33
aaron <aaronkornish.ak@gmail.com>
2024-01-31 12:56:46 -0800

    Created the book repository class implementing the book repository interface

---
+++ BookBazaar/Models/EFBookRepository.cs
@@ -0,0 +1,19 @@
+﻿
+using BookBazaar.Data;
+
+namespace BookBazaar.Models
+{
+    // This is the model for the Book table in the database
+    public class EFBookRepository : IBookRepository
+    {
+        private ApplicationDbContext _context;
+
+        // This is the constructor for the EFBookRepository
+        public EFBookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Book> Books => _context.Books;
+    }
+}
