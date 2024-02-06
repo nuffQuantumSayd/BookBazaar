ab3fa9b6d1df859ca9d0945ebfc4eb0fef7eb50b
aaron <aaronkornish.ak@gmail.com>
2024-02-06 15:42:56 -0800

    Added logic to index in home controller for a book search by title

--- BookBazaar/Controllers/HomeController.cs
+++ BookBazaar/Controllers/HomeController.cs
@@ -18,9 +18,26 @@ namespace BookBazaar.Controllers
         }
 
         // GET: Books
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {   
-            return View(await _context.Books.ToListAsync());
+            // If the database is empty, return a problem
+            if (_context.Books == null)
+            {
+                return Problem("The database is empty");
+            }
+
+            // If the search string is empty, return all books
+            var books = from b in _context.Books
+                        select b;
+
+            // If the search string is not empty, return books that contain the search string
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(b => b.Title!.Contains(searchString));
+            }
+
+            // Return the view with the books
+            return View(await books.ToListAsync());
         }
 
         public IActionResult Privacy()
