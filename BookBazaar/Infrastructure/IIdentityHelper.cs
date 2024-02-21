f6fb3b5c3ec6af4459f3a6c81aaac907721f9926
aaron <aaronkornish.ak@gmail.com>
2024-02-21 11:50:08 -0800

    Created an identity helper interface

---
+++ BookBazaar/Infrastructure/IIdentityHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace BookBazaar.Infrastructure
+{
+    public interface IIdentityHelper
+    {
+        /// <summary>
+        /// this method is used to create roles
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <param name="roles"></param>
+        /// <returns></returns>
+        public abstract static Task CreateRoles(IServiceProvider provider, params string[] roles);
+
+        /// <summary>
+        /// Create a default user if one does not exist
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public abstract static Task CreateDefaultUser(IServiceProvider provider, string role);
+    }
+}
